f507d2cdd11adb2b744042c932957fbea430f6b4
manar trimeche <manar.trimeche@polytechnicien.tn>
2025-05-07 17:53:57 +0100

    Premier envoi du projet

---
+++ source/repos/GestionDeStock/CategoryForms/Form_Menu.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace GestionDeStock.CategoryForms
+{
+    partial class Form_Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(41, 39, 40);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(238, 553);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(178, 8, 55);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(238, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(642, 15);
+            panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Image = Properties.Resources.Shutdown_32;
+            button1.Location = new Point(838, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(42, 29);
+            button1.TabIndex = 2;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form_Menu
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(880, 553);
+            Controls.Add(button1);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form_Menu";
+            Text = "Form_Menu";
+            Load += Form_Menu_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Button button1;
+    }
+}
